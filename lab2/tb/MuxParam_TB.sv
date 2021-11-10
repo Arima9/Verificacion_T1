6d9958f0433dbbd12de9c3472a6855095f6837de
Andres Rivera Marquez <andres.mrquez16@gmail.com>
2021-11-09 22:48:17 -0700

    Edicion del archivo testbench del LAB2

--- lab2/tb/MuxParam_TB.sv
+++ lab2/tb/MuxParam_TB.sv
@@ -13,6 +13,31 @@ MuxParam_pkg::mem_ctl_st_t ResultSt;
 MuxParam #(.T(MuxParam_pkg::op_codes_e_t), .SEL(Selector)) DUT0(.Entry(Var_Enum), .Select(Sel), .Sal(ResultEn));
 MuxParam #(.T(MuxParam_pkg::mem_ctl_st_t), .SEL(Selector)) DUT1(.Entry(Var_Stru), .Select(Sel), .Sal(ResultSt));
 
+task check_MuxParam_Enum;
+input Aenum, Bstru, Sel, EnumMux, StruMux;
+MuxParam_pkg::op_codes_e_t Aenum[2**Selector];
+MuxParam_pkg::mem_ctl_st_t Bstru[2**Selector];
+logic [Selector-1:0] Sel;
+MuxParam_pkg::op_codes_e_t EnumMux;
+MuxParam_pkg::mem_ctl_st_t StruMux;
+
+MuxParam_pkg::op_codes_e_t ResultEnum_Gold;
+MuxParam_pkg::mem_ctl_st_t ResultStru_Gold;
+
+begin
+	ResultEnum_Gold = Aenum[Sel];
+	ResultStru_Gold = Bstru[Sel];
+	
+	if (EnumMux != ResultEnum_Gold)
+		$display($time, "Error en el MUX de tipo Enumerado. Valor Obtenido %s, Valor Esperado %s", EnumMux, ResultEnum_Gold);
+		$stop(1);
+	if (StruMux != ResultStru_Gold)
+		$display($time, "Error en el MUX de tipo Estructura. Valor Obtenido ", StruMux, ", Valor Esperado", ResultStru_Gold;
+		$stop(1);
+end
+
+endtask
+
 
 
 endmodule
